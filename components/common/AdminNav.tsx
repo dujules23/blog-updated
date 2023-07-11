c28b2d4320e8128a7b8845171764a38842098597
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-07-11 11:50:43 -0400

    created navItems for AdminNav

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import { FC } from "react";
 import Logo from "./Logo";
+import { IconType } from "react-icons";
 
-interface Props {}
+interface Props {
+  navItems: { label: string; icon: IconType; href: string }[];
+}
 
-const AdminNav: FC<Props> = (props): JSX.Element => {
+const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
   return (
     <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark ">
       {/* Logo */}
       <Link href="/admin">
-        <div className="flex items-center space-x-2 p-3 ">
+        <div className="flex items-center space-x-2 p-3 mb-3">
           <Logo className="fill-highlight-light dark:fill-highlight-dark w-5 h-5" />
           <span className="text-highlight-light dark:text-highlight-dark w-5 h-5 text-xl font-semibold mb-1">
             Admin
@@ -18,6 +21,19 @@ const AdminNav: FC<Props> = (props): JSX.Element => {
       </Link>
 
       {/* Nav Items */}
+
+      <div className="space-y-6">
+        {navItems.map((item) => {
+          return (
+            <Link key={item.href} href={item.href}>
+              <div className="flex items-center text-highlight-light dark:text-highlight-dark  text-xl p-3 hover:scale-[0.98] transition">
+                <item.icon size={24} />
+                <span className="ml-2">{item.label}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
 
       {/* Nav Toggle (button) */}
     </nav>
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,27 @@
 import AdminNav from "@/components/common/AdminNav";
 import { NextPage } from "next";
+import {
+  AiOutlineContacts,
+  AiOutlineContainer,
+  AiOutlineDashboard,
+  AiOutlineMail,
+  AiOutlineTeam,
+} from "react-icons/ai";
 
 interface Props {}
+
+const navItems = [
+  { href: "/admin", icon: AiOutlineDashboard, label: "Dashboard" },
+  { href: "/admin/posts", icon: AiOutlineContainer, label: "Posts" },
+  { href: "/admin/users", icon: AiOutlineTeam, label: "Users" },
+  { href: "/admin/comments", icon: AiOutlineMail, label: "Comments" },
+  { href: "/admin/contact", icon: AiOutlineContacts, label: "Contact" },
+];
 
 const Admin: NextPage<Props> = () => {
   return (
     <div>
-      <AdminNav />
+      <AdminNav navItems={navItems} />
     </div>
   );
 };
