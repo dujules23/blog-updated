5fc34c5c4d854f26b503a0270d7d412030350a91
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-07-11 12:05:50 -0400

    added toggle menu button

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { FC } from "react";
 import Logo from "./Logo";
 import { IconType } from "react-icons";
+import { RiMenuFoldFill } from "react-icons/ri";
 
 interface Props {
   navItems: { label: string; icon: IconType; href: string }[];
@@ -9,33 +10,39 @@ interface Props {
 
 const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
   return (
-    <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark ">
-      {/* Logo */}
-      <Link href="/admin">
-        <div className="flex items-center space-x-2 p-3 mb-3">
-          <Logo className="fill-highlight-light dark:fill-highlight-dark w-5 h-5" />
-          <span className="text-highlight-light dark:text-highlight-dark w-5 h-5 text-xl font-semibold mb-1">
-            Admin
-          </span>
+    <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between ">
+      <div>
+        {/* Logo */}
+        <Link href="/admin">
+          <div className="flex items-center space-x-2 p-3 mb-3">
+            <Logo className="fill-highlight-light dark:fill-highlight-dark w-5 h-5" />
+            <span className="text-highlight-light dark:text-highlight-dark w-5 h-5 text-xl font-semibold mb-1">
+              Admin
+            </span>
+          </div>
+        </Link>
+
+        {/* Nav Items */}
+
+        <div className="space-y-6">
+          {navItems.map((item) => {
+            return (
+              <Link key={item.href} href={item.href}>
+                <div className="flex items-center text-highlight-light dark:text-highlight-dark  text-xl p-3 hover:scale-[0.98] transition">
+                  <item.icon size={24} />
+                  <span className="ml-2">{item.label}</span>
+                </div>
+              </Link>
+            );
+          })}
         </div>
-      </Link>
-
-      {/* Nav Items */}
-
-      <div className="space-y-6">
-        {navItems.map((item) => {
-          return (
-            <Link key={item.href} href={item.href}>
-              <div className="flex items-center text-highlight-light dark:text-highlight-dark  text-xl p-3 hover:scale-[0.98] transition">
-                <item.icon size={24} />
-                <span className="ml-2">{item.label}</span>
-              </div>
-            </Link>
-          );
-        })}
       </div>
 
       {/* Nav Toggle (button) */}
+
+      <button className="text-highlight-light dark:text-highlight-dark p-3 hover:scale-[0.98] transition self-end">
+        <RiMenuFoldFill size={25} />
+      </button>
     </nav>
   );
 };
