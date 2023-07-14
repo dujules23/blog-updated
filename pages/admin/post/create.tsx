99478c28e841b4460eedd639de9d95cf29414cea
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-07-14 01:48:32 -0400

    installed tiptap editor, rendered basic editor

---
+++ pages/admin/post/create.tsx
@@ -0,0 +1,10 @@
+import Editor from "@/components/editor";
+import { NextPage } from "next";
+
+interface Props {}
+
+const Create: NextPage<Props> = () => {
+  return <Editor />;
+};
+
+export default Create;
