17edff6c898e7c3c7254dd11e2cfee92d3052786
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-07-26 23:37:07 -0400

    edited modal close functionality

--- components/common/ModalContainer.tsx
+++ components/common/ModalContainer.tsx
@@ -1,4 +1,11 @@
-import { FC, ReactNode } from "react";
+import {
+  FC,
+  MouseEventHandler,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useId,
+} from "react";
 
 export interface ModalProps {
   visible?: boolean;
@@ -14,14 +21,26 @@ const ModalContainer: FC<Props> = ({
   children,
   onClose,
 }): JSX.Element | null => {
-  if (!visible) return null;
-
-  const handleClick = () => {
+  const containerId = useId();
+  const handleClose = useCallback(() => {
     onClose && onClose();
+  }, [onClose]);
+
+  const handleClick = ({ target }: any) => {
+    if (target.id === containerId) handleClose();
   };
+
+  useEffect(() => {
+    const closeModal = ({ key }: any) => key === "Escape" && handleClose();
+    document.addEventListener("keydown", closeModal);
+    return () => document.removeEventListener("keydown", closeModal);
+  }, [handleClose]);
+
+  if (!visible) return null;
 
   return (
     <div
+      id={containerId}
       onClick={handleClick}
       className="fixed inset-0 bg-primary dark:bg-primary-dark dark:bg-opacity-5 bg-opacity-5 backdrop-blur-[2px] z-50 flex items-center justify-center"
     >
