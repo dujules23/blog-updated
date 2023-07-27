e2ffd876160ef8985f64fab3d269c958a68f6661
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-07-26 23:49:31 -0400

    added custom css for scroll bar, made Gallery component

--- components/editor/GalleryModal/index.tsx
+++ components/editor/GalleryModal/index.tsx
@@ -1,13 +1,23 @@
 import ModalContainer, { ModalProps } from "@/components/common/ModalContainer";
 import { FC } from "react";
+import Gallery from "./Gallery";
 
 interface Props extends ModalProps {}
 
 const GalleryModal: FC<Props> = ({ visible, onClose }): JSX.Element => {
   return (
     <ModalContainer visible={visible} onClose={onClose}>
-      <div className="bg-black p-20">
-        <button className="bg-white p-3">Click Me</button>
+      <div className="max-w-4xl p-2 bg-primary-dark dark:bg-primary rounded">
+        <div className="flex">
+          {/* gallery */}
+
+          <div className="basis-[75%] max-h-[450px] overflow-y-auto custom-scroll-bar">
+            <Gallery />
+          </div>
+
+          {/* image selection and upload */}
+          <div className="basis-1/4"></div>
+        </div>
       </div>
     </ModalContainer>
   );
