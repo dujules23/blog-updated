7f1f815cd1e5ec4ef7ec04b55f067bed7cd196cd
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-08-03 22:42:29 -0400

    added image.ts api file

---
+++ pages/api/image.ts
@@ -0,0 +1,32 @@
+import { NextApiHandler } from "next";
+import formidable from "formidable";
+import { v2 as cloudinary } from "cloudinary";
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+const handler: NextApiHandler = (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      return uploadNewImage(req, res);
+    case "GET":
+      return readAllImages(req, res);
+    default:
+      return res.status(404).send("Not Found!");
+  }
+};
+
+const uploadNewImage: NextApiHandler = (req, res) => {
+  const form = formidable();
+  form.parse(req, (err, fields, files) => {
+    if (err) return res.status(500).json({ error: err.message });
+
+    const imageFile = files.image as formidable.File;
+  });
+};
+const readAllImages: NextApiHandler = (req, res) => {};
+
+export default handler;
