45c22d653936ee2d5fc8c4bd605dbce3e9fa5669
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-08-05 01:01:34 -0400

    created Seo Form state, works as intended

--- components/editor/SeoForm.tsx
+++ components/editor/SeoForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, FC } from "react";
+import { ChangeEventHandler, FC, useState } from "react";
 import classNames from "classnames";
 
 interface Props {}
@@ -7,20 +7,50 @@ const commonInput =
   "w-full bg-transparent outline-none border-2 border-secondary-dark focus:border-primary-dark focus:dark:border-primary rounded transition text-primary-dark dark:text-primary p-2 ";
 
 const SeoForm: FC<Props> = (props): JSX.Element => {
+  const [values, setValues] = useState({ meta: "", slug: "", tags: "" });
+
+  const handleChange: ChangeEventHandler<
+    HTMLTextAreaElement | HTMLInputElement
+  > = ({ target }) => {
+    let { name, value } = target;
+
+    if (name === "meta") value = value.substring(0, 150);
+
+    setValues({ ...values, [name]: value });
+
+    console.log(target.name);
+  };
+
+  const { meta, slug, tags } = values;
   return (
     <div className="space-y-4">
       <h1 className="text-primary-dark dark:text-primary text-xl font-semibold">
         SEO Section
       </h1>
-      <Input name="slug" placeholder="slug-goes-here" label="Slug:" />
-      <Input name="tags" placeholder="React, Next JS" label="Tags:" />
+      <Input
+        onChange={handleChange}
+        name="slug"
+        value={slug}
+        placeholder="slug-goes-here"
+        label="Slug:"
+      />
+      <Input
+        onChange={handleChange}
+        name="tags"
+        value={tags}
+        placeholder="React, Next JS"
+        label="Tags:"
+      />
       <div className="relative">
         <textarea
+          onChange={handleChange}
+          name="meta"
+          value={meta}
           className={classNames(commonInput, "text-lg h-20 resize-none")}
           placeholder="Meta description"
         ></textarea>
         <p className="absolute bottom-3 right-3 text-primary-dark dark:text-primary text-sm">
-          0/150
+          {meta.length}/150
         </p>
       </div>
     </div>
