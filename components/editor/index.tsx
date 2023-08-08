b4926cf2859d8efc40f2867995d197903b7959f4
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-08-08 15:43:09 -0400

    setup change handlers for editor

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { ChangeEventHandler, FC, useEffect, useState } from "react";
 import { useEditor, EditorContent, getMarkRange, Range } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import ToolBar from "./ToolBar";
@@ -10,17 +10,32 @@ import TipTapImage from "@tiptap/extension-image";
 import EditLink from "./Link/EditLink";
 import GalleryModal, { ImageSelectionResult } from "./GalleryModal";
 import axios from "axios";
-import SeoForm from "./SeoForm";
+import SeoForm, { SeoResult } from "./SeoForm";
 import ActionButton from "../common/ActionButton";
 import ThumbnailSelector from "./ThumbnailSelector";
 
-interface Props {}
+interface FinalPost extends SeoResult {
+  title: string;
+  content: string;
+  thumbnail?: File | string;
+}
 
-const Editor: FC<Props> = (): JSX.Element => {
+interface Props {
+  onSubmit(post: FinalPost): void;
+}
+
+const Editor: FC<Props> = ({ onSubmit }): JSX.Element => {
   const [selectionRange, setSelectionRange] = useState<Range>();
   const [showGallery, setShowGallery] = useState(false);
   const [uploading, setUploading] = useState(false);
   const [images, setImages] = useState<{ src: string }[]>([]);
+  const [post, setPost] = useState<FinalPost>({
+    title: "",
+    content: "",
+    meta: "",
+    tags: "",
+    slug: "",
+  });
 
   const fetchImages = async () => {
     const { data } = await axios("/api/image");
@@ -90,6 +105,19 @@ const Editor: FC<Props> = (): JSX.Element => {
       .run();
   };
 
+  const handleSubmit = () => {
+    if (!editor) return;
+    onSubmit({ ...post, content: editor.getHTML() });
+  };
+
+  // onChange functions
+  const updateTitle: ChangeEventHandler<HTMLInputElement> = ({ target }) =>
+    setPost({ ...post, title: target.value });
+
+  const updateSeoValue = (result: SeoResult) => setPost({ ...post, ...result });
+
+  const updateThumbnail = (file: File) => setPost({ ...post, thumbnail: file });
+
   useEffect(() => {
     if (editor && selectionRange) {
       editor.commands.setTextSelection(selectionRange);
@@ -106,9 +134,9 @@ const Editor: FC<Props> = (): JSX.Element => {
         <div className="sticky top-0 z-10 dark:bg-primary-dark bg-primary">
           {/* Thumbnail Selector and Submit Button */}
           <div className="flex items-center justify-between mb-3">
-            <ThumbnailSelector onChange={(file) => console.log(file)} />
+            <ThumbnailSelector onChange={updateThumbnail} />
             <div className="inline-block">
-              <ActionButton title="Submit" />
+              <ActionButton title="Submit" onClick={handleSubmit} />
             </div>
           </div>
 
@@ -117,6 +145,7 @@ const Editor: FC<Props> = (): JSX.Element => {
             type="text"
             className="py-2 outline-none bg-transparent w-full border-0 border-b-[1px] border-secondary-dark dark:border-secondary-light text-3xl font-semibold italic text-primary-dark dark:text-primary mb-3"
             placeholder="Title"
+            onChange={updateTitle}
           />
           <ToolBar
             editor={editor}
@@ -129,7 +158,7 @@ const Editor: FC<Props> = (): JSX.Element => {
         <EditorContent editor={editor} className="min-h-[300px]" />
         <div className="h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3"></div>
         {/* SEO Section */}
-        <SeoForm onChange={(result) => console.log(result)} />
+        <SeoForm onChange={updateSeoValue} title={post.title} />
       </div>
 
       {/* Gallery Modal () */}
