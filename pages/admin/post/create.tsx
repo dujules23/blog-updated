525b7708104edd13d86cb0d4708aaf4c63e4b0b7
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-08-14 23:25:31 -0400

    added new favicon, created AppHead, updated AdminLayout and added to create.tsx

--- pages/admin/post/create.tsx
+++ pages/admin/post/create.tsx
@@ -1,13 +1,16 @@
 import Editor from "@/components/editor";
+import AdminLayout from "@/components/layout/AdminLayout";
 import { NextPage } from "next";
 
 interface Props {}
 
 const Create: NextPage<Props> = () => {
   return (
-    <div className="max-w-4xl mx-auto">
-      <Editor onSubmit={(post) => console.log(post)} />
-    </div>
+    <AdminLayout title="New Post">
+      <div className="max-w-4xl mx-auto">
+        <Editor onSubmit={(post) => console.log(post)} />
+      </div>
+    </AdminLayout>
   );
 };
 
