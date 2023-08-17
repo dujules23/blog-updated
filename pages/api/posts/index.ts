69bc6b0f762005f124812e6954e2501f3c5711ca
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-08-17 00:44:35 -0400

    began Joi validation

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,4 +1,5 @@
 import dbConnect from "@/lib/dbConnext";
+import Joi from "joi";
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
@@ -8,7 +9,32 @@ const handler: NextApiHandler = async (req, res) => {
       await dbConnect();
       res.json({ ok: true });
     }
+    case "POST":
+      return createNewPost(req, res);
   }
+};
+
+const createNewPost: NextApiHandler = (req, res) => {
+  const { body } = req;
+
+  const schema = Joi.object().keys({
+    title: Joi.string().required().messages({
+      "string.empty": "Title cannot be empty!",
+      "any.required": "Title is a required field",
+    }),
+    content: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(body, {
+    errors: { label: "key", wrap: { label: false, array: false } },
+  });
+  console.log(error);
+  if (error) {
+    const errorMessage = error.details[0].message;
+    return res.status(400).json({ error: errorMessage });
+  }
+
+  res.json({ ok: true });
 };
 
 export default handler;
