03c66bb2e8f3fd89e9c4766c9f3293d2e497aae7
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-08-17 20:59:03 -0400

    created handleSumbit, works but backend is not handling data correctly

--- pages/admin/post/create.tsx
+++ pages/admin/post/create.tsx
@@ -1,14 +1,34 @@
-import Editor from "@/components/editor";
+import Editor, { FinalPost } from "@/components/editor";
 import AdminLayout from "@/components/layout/AdminLayout";
+import axios from "axios";
 import { NextPage } from "next";
 
 interface Props {}
 
 const Create: NextPage<Props> = () => {
+  const handleSubmit = async (post: FinalPost) => {
+    // we have to generate FormData
+    const formData = new FormData();
+
+    for (let key in post) {
+      const value = (post as any)[key];
+      if (key === "tags") {
+        // loops through array to remove blank spaces
+        const tags = value.split(",").map((item: string) => item.trim());
+        formData.append("tags", JSON.stringify(tags));
+      } else formData.append(key, value);
+    }
+    // submit our post
+
+    const { data } = await axios.post("/api/posts", formData);
+
+    console.log(data);
+  };
+
   return (
     <AdminLayout title="New Post">
       <div className="max-w-4xl mx-auto">
-        <Editor onSubmit={(post) => console.log(post)} />
+        <Editor onSubmit={handleSubmit} />
       </div>
     </AdminLayout>
   );
