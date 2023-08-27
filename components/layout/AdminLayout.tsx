18da75806c6594505f75e45d43f7d1f2f47e50b5
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-08-26 22:56:14 -0400

    cleand up layout of admin folder, updated AdminLayout

--- components/layout/AdminLayout.tsx
+++ components/layout/AdminLayout.tsx
@@ -32,7 +32,7 @@ const AdminLayout: FC<Props> = ({ children, title }): JSX.Element => {
         <AdminNav navItems={navItems} />
         <div className="flex-1 p-4">{children}</div>
         {/* create button */}
-        <Link href="/admin/post/create">
+        <Link href="/admin/posts/create">
           <div className="bg-secondary-dark dark:bg-secondary-light text-primary dark:text-primary-dark fixed z-10 right-10 bottom-10 p-3 rounded-full hover:scale-90 shadow-sm transition">
             <AiOutlineFileAdd size={24} />
           </div>
---
+++ pages/admin/posts/update/[slug].tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const Update: NextPage<Props> = () => {
+  return <div>Update</div>;
+};
+
+export default Update;
