8a2994eeb25999d4a8887201ca73f15169d458ad
Durrell Jules <dujules23@gmail.com>
2023-09-01 16:38:33 -0400

    setup posts in home page with pagination, works as intended

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,86 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-import { NextPage } from "next";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 import DefaultLayout from "@/components/layout/DefaultLayout";
+import { formatPosts, readPostsFromDb } from "@/lib/utils";
+import { PostDetail } from "@/utils/types";
+import InfiniteScrollPosts from "@/components/common/InfiniteScrollPosts";
+import { useState } from "react";
+import axios from "axios";
 
-const Home: NextPage = () => {
-  return <DefaultLayout>Home</DefaultLayout>;
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+const Home: NextPage<Props> = ({ posts }) => {
+  const [postsToRender, setPostsToRender] = useState(posts);
+  const [hasMorePosts, setHasMorePosts] = useState(true);
+
+  const isAdmin = false;
+  // pagination function
+  const fetchMorePosts = async () => {
+    try {
+      // increases page number
+      pageNo++;
+      // api call using page number and limit to get next page of data
+      const { data } = await axios(
+        `/api/posts?limit=${limit}&pageNo=${pageNo}`
+      );
+      // checks to see if the length of posts are less than 9 (limit), we ran out of posts in the database
+      if (data.posts.length < limit) {
+        // sets posts, as we scroll down we add the posts that were brought in through the endpoint
+        setPostsToRender([...postsToRender, ...data.posts]);
+        setHasMorePosts(false);
+      } else {
+        setPostsToRender([...postsToRender, ...data.posts]);
+      }
+    } catch (error) {
+      setHasMorePosts(false);
+      console.log(error);
+    }
+  };
+
+  return (
+    <DefaultLayout>
+      <div className="pb-20"></div>
+      <InfiniteScrollPosts
+        hasMore={hasMorePosts}
+        next={fetchMorePosts}
+        dataLength={postsToRender.length}
+        posts={postsToRender}
+        showControls={isAdmin}
+      />
+    </DefaultLayout>
+  );
+};
+
+interface ServerSideResponse {
+  posts: PostDetail[];
+}
+
+let pageNo = 0;
+const limit = 9;
+
+export const getServerSideProps: GetServerSideProps<
+  ServerSideResponse
+> = async () => {
+  try {
+    // read posts
+    const posts = await readPostsFromDb(limit, pageNo);
+
+    // format posts
+    const formattedPosts = formatPosts(posts);
+    return {
+      props: {
+        posts: formattedPosts,
+      },
+    };
+  } catch (error) {
+    return { notFound: true };
+  }
 };
 
 export default Home;
