d970203eebdbf65998f240aa08780ba95a8e7644
Durrell Jules <dujules23@gmail.com>
2023-09-05 00:36:39 -0400

    setup profile using User table, successufully adds User to database

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,3 +1,5 @@
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/User";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GitHubAuthProvider from "next-auth/providers/github";
 
@@ -6,6 +8,24 @@ const authOptions: NextAuthOptions = {
     GitHubAuthProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      async profile(profile) {
+        // find out the user
+        await dbConnect();
+        const oldUser = await User.findOne({ email: profile.email });
+        // store new user inside db
+        if (!oldUser) {
+          const newUser = new User({
+            email: profile.email,
+            name: profile.name || profile.login,
+            provider: "github",
+            avatar: profile.avatar_url,
+          });
+
+          await newUser.save();
+        }
+
+        return profile;
+      },
     }),
   ],
 };
