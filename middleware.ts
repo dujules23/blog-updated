74a67d14370966f6baf1309821f075343a8a854c
Durrell Jules <dujules23@gmail.com>
2023-09-05 16:46:36 -0400

    began working on middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    // navigate our user
+    console.log(req);
+    NextResponse.rewrite(new URL(req.url));
+  },
+  {
+    callbacks: {
+      authorized({ token }) {
+        console.log(token);
+        return token?.role === "admin";
+      },
+    },
+  }
+);
+
+export const config = { matcher: ["/admin"] };
