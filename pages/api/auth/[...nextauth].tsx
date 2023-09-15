5263c90a869cd5f69f692bb07d50a9c7f86cfe4f
Durrell Jules <dujules23@gmail.com>
2023-09-15 01:07:11 -0400

    updated dev and production variables

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -3,11 +3,24 @@ import User from "@/models/User";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GitHubAuthProvider from "next-auth/providers/github";
 
+const {
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  GITHUB_CLIENT_SECRET_LOCAL,
+  GITHUB_CLIENT_ID_LOCAL,
+  MODE,
+} = process.env;
+
+const GIT_CLIENT_ID =
+  MODE === "development" ? GITHUB_CLIENT_ID_LOCAL : GITHUB_CLIENT_ID;
+const GIT_CLIENT_SECRET =
+  MODE === "development" ? GITHUB_CLIENT_SECRET_LOCAL : GITHUB_CLIENT_SECRET;
+
 export const authOptions: NextAuthOptions = {
   providers: [
     GitHubAuthProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: GIT_CLIENT_ID as string,
+      clientSecret: GIT_CLIENT_SECRET as string,
       async profile(profile) {
         // find out the user
         console.log(profile);
