7ed2298967d51b2702b2d807371290c7d9edd668
Durrell Jules <dujules23@gmail.com>
2023-09-17 23:55:21 -0400

    finished createNewComment endpoint

--- pages/api/comment/index.ts
+++ pages/api/comment/index.ts
@@ -1,5 +1,8 @@
+import dbConnect from "@/lib/dbConnect";
 import { isAuth } from "@/lib/utils";
 import { commentValidationSchema, validateSchema } from "@/lib/validator";
+import Comment from "@/models/Comment";
+import Post from "@/models/Post";
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = (req, res) => {
@@ -21,6 +24,22 @@ const createNewComment: NextApiHandler = async (req, res) => {
 
   const error = validateSchema(commentValidationSchema, req.body);
   if (error) return res.status(422).json({ error });
+
+  // creating the comment
+  await dbConnect();
+  const { belongsTo, content } = req.body;
+  const post = await Post.findById(belongsTo);
+  if (!post) return res.status(401).json({ error: "Invalid Post" });
+
+  const comment = new Comment({
+    content,
+    belongsTo,
+    owner: "64f6af75a16d819b21b7adb0",
+    chiefComment: true,
+  });
+
+  await comment.save();
+  res.status(201).json(comment);
 };
 
 export default handler;
