102d07c0e93a7870db5480ab4c3fddf6cb82e11c
Durrell Jules <dujules23@gmail.com>
2023-09-18 17:11:12 -0400

    completed update-like endpoint, tested in postman, works as intended

--- pages/api/comment/update-like.ts
+++ pages/api/comment/update-like.ts
@@ -19,18 +19,37 @@ const handler: NextApiHandler = (req, res) => {
 
 // creating new comment, checks if the user is authorized, if not, returns 403 status
 const updateLike: NextApiHandler = async (req, res) => {
+  // checks for authentication
   const user = await isAuth(req, res);
   if (!user) return res.status(403).json({ error: "unauthorized request!" });
 
   const { commentId } = req.body;
-
+  // checks to see if the comment id is valid
   if (!isValidObjectId(commentId))
     return res.status(422).json({ error: "invalid comment id!" });
-
+  // connects to database
   await dbConnect();
 
   const comment = await Comment.findById(commentId);
+
   if (!comment) return res.status(404).json({ error: "Comment not found!" });
+
+  const oldLikes = comment.likes || [];
+  const likedBy = user.id as any;
+
+  // like and unlike
+  // this is for unlike
+  if (oldLikes.includes(likedBy)) {
+    // filters through old likes, if the like is equal to the liked by user, it will be removed from likes
+    comment.likes = oldLikes.filter(
+      (like) => like.toString() !== likedBy.toString()
+    );
+  }
+  // this is to like comment
+  else comment.likes = [...oldLikes, likedBy];
+
+  await comment.save();
+  res.status(201).json({ comment });
 };
 
 export default handler;
