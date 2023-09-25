0c361efb7eae5586adc73fdd3784a3ac5216572b
Durrell Jules <dujules23@gmail.com>
2023-09-25 16:47:01 -0400

    connected endpoints for latests posts and latest comments, renders correctly in the admin dashboard

--- components/admin/ContentWrapper.tsx
+++ components/admin/ContentWrapper.tsx
@@ -17,7 +17,7 @@ const ContentWrapper: FC<Props> = ({
       <h3 className="text-2xl dark:text-primary text-primary-dark font-semibold py-2 transition">
         {title}
       </h3>
-      <div className="border-2 border-secondary-dark rounded p-3 flex-1">
+      <div className="border-2 border-secondary-dark rounded p-3 flex-1 flex flex-col justify-between">
         <div className="space-y-5">{children}</div>
         <div className="mt-2 text-right self-end">
           <Link href={seeAllRoute}>
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -3,23 +3,46 @@ import LatestCommentListCard from "@/components/admin/LatestCommentListCard";
 import LatestPostListCard from "@/components/admin/LatestPostListCard";
 import AdminNav from "@/components/common/nav/AdminNav";
 import AdminLayout from "@/components/layout/AdminLayout";
+import { LatestComment, PostDetail } from "@/utils/types";
+import axios from "axios";
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 interface Props {}
 
 const Admin: NextPage<Props> = () => {
+  const [latestPosts, setLatestPosts] = useState<PostDetail[]>();
+  const [latestComments, setLatestComments] = useState<LatestComment[]>();
+
+  useEffect(() => {
+    // fetching latest posts
+    axios("/api/posts?limit=5&skip=0")
+      .then(({ data }) => setLatestPosts(data.posts))
+      .catch((err) => console.log(err));
+    // fetching latest comments
+    axios("/api/comment/latest")
+      .then(({ data }) => setLatestComments(data.comments))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <AdminLayout>
       <div className="flex space-x-10">
-        <ContentWrapper seeAllRoute="/admin" title="Latest Posts">
-          <LatestPostListCard
-            title="This is my Title"
-            slug="this is the slug"
-            meta="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora consectetur iste quod dolore, voluptate minima excepturi ipsa labore quae, temporibus eum eaque vel laborum sed fugiat, rem consequatur. Consectetur unde quis, expedita sunt odit temporibus dolore enim optio voluptas nihil dignissimos pariatur nemo impedit iusto amet blanditiis harum quia? Quasi?"
-          />
+        <ContentWrapper seeAllRoute="/admin/posts" title="Latest Posts">
+          {latestPosts?.map(({ id, title, meta, slug }) => {
+            return (
+              <LatestPostListCard
+                key={id}
+                title={title}
+                meta={meta}
+                slug={slug}
+              />
+            );
+          })}
         </ContentWrapper>
         <ContentWrapper seeAllRoute="/admin" title="Latest Comments">
-          {/* <LatestCommentListCard comment={} /> */}
+          {latestComments?.map((comment) => {
+            return <LatestCommentListCard key={comment.id} comment={comment} />;
+          })}
         </ContentWrapper>
       </div>
     </AdminLayout>
