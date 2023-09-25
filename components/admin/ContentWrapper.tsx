bd47fac60c0b7e07d70f67bc238b3e99557f7f86
Durrell Jules <dujules23@gmail.com>
2023-09-24 22:30:19 -0400

    created ContentWrapper for Admin dashboard

---
+++ components/admin/ContentWrapper.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { FC, ReactNode } from "react";
+
+interface Props {
+  title: string;
+  children: ReactNode;
+  seeAllRoute: string;
+}
+
+const ContentWrapper: FC<Props> = ({
+  title,
+  children,
+  seeAllRoute,
+}): JSX.Element => {
+  return (
+    <div className="flex flex-col min-w-[300px]">
+      <h3 className="text-2xl dark:text-primary text-primary-dark font-semibold py-2 transition">
+        {title}
+      </h3>
+      <div className="border-2 border-secondary-dark rounded p-3 flex-1">
+        <div className="space-y-5">{children}</div>
+        <div className="mt-2 text-right self-end">
+          <Link href={seeAllRoute}>
+            <div className="text-primary-dark dark:text-primary hover:underline transition">
+              See All
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContentWrapper;
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,3 +1,4 @@
+import ContentWrapper from "@/components/admin/ContentWrapper";
 import AdminNav from "@/components/common/nav/AdminNav";
 import AdminLayout from "@/components/layout/AdminLayout";
 import { NextPage } from "next";
@@ -7,116 +8,13 @@ interface Props {}
 const Admin: NextPage<Props> = () => {
   return (
     <AdminLayout>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis
-        placeat asperiores molestiae deserunt illo esse soluta culpa hic natus
-        amet facere minima harum exercitationem numquam adipisci eos autem
-        dolores, minus labore doloribus vitae! Illum asperiores delectus itaque
-        iste tempora natus earum assumenda corporis suscipit soluta rem expedita
-        nam amet reiciendis dolore ipsa, nostrum ut incidunt aliquam voluptatum.
-        Asperiores quo nobis ex optio officiis quaerat, enim beatae
-        exercitationem ea quisquam, porro tempore praesentium deserunt cumque
-        aut vitae aliquid amet nostrum odio id consequatur culpa sequi ut
-        accusantium? Commodi perferendis ullam rerum rem ducimus molestias
-        dolores illo minima modi obcaecati. Dignissimos, hic tenetur officiis
-        blanditiis eos iste. Voluptatem, vitae quae aliquid rem tenetur
-        similique perferendis vero magnam numquam aut nobis quas, tempore qui ut
-        hic, eius quo nihil error veniam repudiandae cupiditate molestias nulla
-        nesciunt culpa! Fuga eligendi, necessitatibus enim dolor voluptas
-        consequuntur veniam similique. Esse tempore voluptatibus magnam.
-        Obcaecati dolor perspiciatis, natus quo esse expedita, impedit quam nam
-        unde praesentium ex ratione, recusandae distinctio eius rem rerum quos
-        laudantium eveniet quas? Id esse cumque dolorem perferendis! Deserunt
-        placeat expedita doloribus at officia neque consectetur ipsum quos
-        voluptatibus blanditiis, aspernatur soluta alias, aut quod inventore ut
-        nobis. Earum voluptas temporibus vitae nesciunt sequi? Inventore
-        incidunt voluptatum tempore repellat quos. Voluptatibus earum tempore
-        iste dolor explicabo et illum atque impedit provident consequatur
-        debitis voluptatem dolorum, nobis laborum. Sequi voluptates beatae
-        blanditiis, a earum impedit repudiandae temporibus cumque, maxime enim,
-        corrupti doloremque explicabo cupiditate aut necessitatibus odio
-        perspiciatis unde voluptate ea sint! Nam, maiores! Magnam a magni nisi
-        perspiciatis autem dolore? Labore deserunt quos, voluptatibus a et
-        dolorem voluptatem, minus quia esse nemo aut ad? Quos commodi, nobis
-        velit aut provident minus maiores inventore quaerat debitis animi sed
-        voluptates! Soluta quibusdam architecto quisquam labore, quasi facere
-        possimus placeat ullam aut velit ea magni enim quod distinctio
-        reprehenderit, explicabo magnam? At similique reprehenderit eum. Aliquam
-        iure eligendi laborum, obcaecati, et a suscipit aut harum numquam
-        reprehenderit, laudantium deleniti quidem nesciunt. Facilis praesentium
-        nulla quibusdam quas optio dignissimos dolorum veniam dolore cum
-        obcaecati fuga blanditiis fugiat, error at inventore animi vitae
-        mollitia eveniet sapiente qui amet doloribus officia aliquid. Dolor
-        eius, placeat autem odit veritatis fugiat. Alias esse vitae distinctio,
-        repellendus accusantium atque culpa labore iure voluptatibus sapiente
-        itaque officia nemo porro, tempora, neque harum deleniti perferendis nam
-        commodi nihil quia inventore sequi! Nisi saepe, a iste numquam expedita
-        necessitatibus voluptatum quisquam voluptates vitae. Doloremque magni
-        repellat animi optio saepe cum possimus, esse et, delectus, quidem nam?
-        Veniam provident a amet necessitatibus, est cupiditate tempora tenetur
-        fugiat aspernatur ducimus at enim ipsam obcaecati ipsum accusamus labore
-        voluptas perspiciatis explicabo quam libero assumenda eaque nemo
-        officiis. Dignissimos possimus corporis, quis magni animi architecto qui
-        amet error nam enim fuga facere, libero ex odio optio consectetur
-        maiores, quam veniam perspiciatis necessitatibus rerum! Voluptate, iure
-        magnam neque, illum distinctio, eius sapiente quam beatae quisquam
-        eveniet rem placeat. Voluptate placeat facere voluptatem, dolores
-        doloremque id repellat quas veniam fugiat natus neque maiores modi iste!
-        Enim distinctio soluta dignissimos iusto voluptates consequuntur
-        sapiente cum id fugiat hic dolore molestias nihil numquam earum saepe
-        est dolor eum ratione, tempore inventore quis velit? Incidunt quas sunt
-        eius quae repellat consectetur reiciendis tenetur, facilis delectus
-        dolorum magnam modi reprehenderit explicabo praesentium. Voluptas,
-        debitis deserunt eum ipsum laborum nihil ea tempore, sit blanditiis
-        accusamus quaerat harum. Eos, enim error perspiciatis eius aperiam
-        numquam dolorum impedit reiciendis voluptates fugiat maxime et odio fuga
-        nam ratione id commodi maiores cum cupiditate totam nulla. Architecto,
-        quo? Incidunt facilis eum illum et quidem modi ex! Dignissimos
-        laboriosam totam optio voluptatibus esse cumque, facere ullam dolorum,
-        expedita molestiae odio quas iusto error quidem officia voluptates
-        debitis mollitia atque earum rerum similique consequuntur? Aperiam quas
-        aspernatur ex totam assumenda dolore accusantium eos deleniti aut
-        suscipit doloribus quia velit nostrum repellendus explicabo officia non
-        excepturi doloremque quisquam, magni, pariatur reiciendis beatae eum.
-        Reiciendis, omnis repudiandae veritatis ut explicabo cum dolores, fugiat
-        facilis minima ea sunt corporis asperiores nam iure earum dolorum iusto
-        quae voluptatum facere illum? Excepturi obcaecati vero quidem id eaque
-        ea similique dolorem ullam incidunt alias, nesciunt, porro repellendus
-        aspernatur esse, maiores accusamus necessitatibus amet a. Sapiente
-        maiores consequatur dolorum aliquid. Qui pariatur aut, quas nam quia
-        voluptatibus necessitatibus praesentium eum perferendis nisi? Magni
-        optio quo, magnam alias itaque earum architecto veniam fugiat natus
-        aperiam mollitia quae praesentium qui ex cum nulla sequi adipisci
-        ratione amet quas ad. Unde porro eius nihil! Ex officia quia eligendi
-        iusto dignissimos veniam pariatur, nisi quo aperiam atque ducimus eius,
-        id maxime fuga repellat suscipit officiis rerum eaque vitae repellendus
-        asperiores. Tempore eaque ipsa voluptate et, error excepturi consectetur
-        qui tenetur, id repellendus voluptates optio aliquid nesciunt!
-        Consequuntur voluptatibus facere est unde sapiente ad veniam, quidem
-        maxime? Corrupti vitae distinctio unde culpa vero praesentium amet
-        doloremque commodi molestiae non quasi provident ab quia et ex magni
-        dignissimos, adipisci minima eum, sapiente sunt iure odit modi. Odit
-        illum numquam iusto neque distinctio pariatur voluptas quos tempore
-        nihil et consectetur soluta voluptatibus exercitationem cum
-        necessitatibus libero, adipisci itaque doloribus! Earum dolor laudantium
-        nulla unde officia? Dolor mollitia voluptate adipisci? Doloribus soluta
-        aperiam iste, optio officia tempora neque ut eius et. Reiciendis
-        explicabo sunt debitis, vero laborum temporibus facere nisi delectus!
-        Quidem ex molestiae voluptatum repellendus numquam quod odio tempore
-        rerum, sit, corrupti reprehenderit aliquam laudantium. Voluptatem
-        laborum necessitatibus error. Blanditiis accusantium ipsam quidem nisi
-        quisquam iure unde facilis voluptatibus harum atque, aspernatur
-        repellendus assumenda sunt, id est corporis explicabo dicta fugit eum
-        quae. Aperiam, enim omnis vel officia minima iusto ut corrupti facere
-        ratione. Accusantium, maxime eaque voluptas itaque saepe enim id, dolor
-        eos sequi eum veritatis reprehenderit esse fugiat culpa quia illo
-        dolorum, labore similique iste reiciendis minus soluta! Doloribus dolore
-        quibusdam ad assumenda dignissimos voluptas cumque neque velit aliquam
-        id reiciendis, necessitatibus non repellat ea alias impedit unde
-        accusantium cum deserunt blanditiis labore, sed qui libero? Delectus sit
-        eligendi architecto aliquam ex. Perspiciatis at repellat totam
-        blanditiis optio est quaerat? Aperiam, illum placeat, quidem veniam, aut
-        eum numquam sapiente culpa nemo a nam!
+      <div className="flex space-x-10">
+        <ContentWrapper seeAllRoute="/admin" title="Latest Posts">
+          <></>
+        </ContentWrapper>
+        <ContentWrapper seeAllRoute="/admin" title="Latest Comments">
+          <></>
+        </ContentWrapper>
       </div>
     </AdminLayout>
   );
