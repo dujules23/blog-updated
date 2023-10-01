e60749cc14ef99eebefd66deb07f31e2ba7f8bfe
Durrell Jules <dujules23@gmail.com>
2023-10-01 16:35:03 -0400

    created share component, rendered icons ffrom next-share

---
+++ components/common/Share.tsx
@@ -0,0 +1,45 @@
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  RedditIcon,
+  RedditShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "next-share";
+import { FC } from "react";
+
+interface Props {
+  url: string;
+  title?: string;
+  quote?: string;
+}
+
+const Share: FC<Props> = ({ url, title, quote }): JSX.Element => {
+  return (
+    <div className="flex items-center space-x-3">
+      <p className="font-semibold text-primary-dark dark:text-primary">
+        Share:
+      </p>
+
+      <FacebookShareButton url={url} quote={quote} title={title}>
+        <FacebookIcon round size={32} />
+      </FacebookShareButton>
+
+      <TwitterShareButton url={url} title={title}>
+        <TwitterIcon round size={32} />
+      </TwitterShareButton>
+
+      <LinkedinShareButton url={url} source={quote} title={title}>
+        <LinkedinIcon round size={32} />
+      </LinkedinShareButton>
+
+      <RedditShareButton url={url} title={title}>
+        <RedditIcon round size={32} />
+      </RedditShareButton>
+    </div>
+  );
+};
+
+export default Share;
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -18,8 +18,11 @@ import { signIn } from "next-auth/react";
 import axios from "axios";
 import User from "@/models/User";
 import AuthorInfo from "@/components/common/AuthorInfo";
+import Share from "@/components/common/Share";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const host = "http://localhost:3000";
 // seems to be an issue in production, check postman for 404 issue
 const SinglePost: NextPage<Props> = ({ post }) => {
   const [likes, setLikes] = useState({ likedByOwner: false, count: 0 });
@@ -83,6 +86,10 @@ const SinglePost: NextPage<Props> = ({ post }) => {
             <span key={t + index}>#{tags}</span>
           ))}
           <span>{dateFormat(createdAt, "d-mmm-yyyy")}</span>
+        </div>
+
+        <div className="py-5 transition dark:bg-primary-dark bg-primary sticky top-0 z-50">
+          <Share url={host + "/" + slug} />
         </div>
 
         <div className="prose prose-lg dark:prose-invert max-w-full mx-auto">
