23e88af624d5dd72cfb4e3774a6e0ad1f5a85709
Durrell Jules <dujules23@gmail.com>
2023-10-02 23:19:48 -0400

    completed request for search api, works as intended

--- pages/admin/search.tsx
+++ pages/admin/search.tsx
@@ -1,9 +1,35 @@
+import AdminLayout from "@/components/layout/AdminLayout";
+import { PostDetail } from "@/utils/types";
+import axios from "axios";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 interface Props {}
 
 const Search: NextPage<Props> = () => {
-  return <div>Search</div>;
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState<PostDetail>([]);
+  const { query } = useRouter();
+  const title = query.title as string;
+
+  const handleSearch = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios("/api/posts/search?title=" + title);
+      setLoading(false);
+      setResults(data.results);
+    } catch (error) {
+      console.log("Error while searching posts: ");
+    }
+  };
+
+  useEffect(() => {
+    if (loading) return;
+    handleSearch();
+  }, [title]);
+
+  return <AdminLayout>Search</AdminLayout>;
 };
 
 export default Search;
