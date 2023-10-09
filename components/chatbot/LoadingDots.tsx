6cb13196c98a538f1063c13a8094a983685612ce
Durrell Jules <dujules23@gmail.com>
2023-10-08 23:51:08 -0400

    made chat bot folder, made LoadingDots component

---
+++ components/chatbot/LoadingDots.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+interface Props {}
+
+const LoadingDots: FC<Props> = (props): JSX.Element => {
+  let circleCommonClasses = "h-1.5 w-1.5 bg-current rounded-full";
+
+  return (
+    <div className="flex text-slate-400">
+      <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
+      <div className={`${circleCommonClasses} mr-1 animate-bounce200`}></div>
+      <div className={`${circleCommonClasses} animate-bounce400`}></div>
+    </div>
+  );
+};
+
+export default LoadingDots;
