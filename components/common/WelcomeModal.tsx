a671b3bab370e1eb0df255a843cb7fdfadb25bd1
Durrell Jules <dujules23@gmail.com>
2023-11-21 01:15:21 -0500

    updated text in Welcome Model, should fix errors

--- components/common/WelcomeModal.tsx
+++ components/common/WelcomeModal.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
 import ModalContainer, { ModalProps } from "./ModalContainer";
 import { Modal } from "@chakra-ui/react";
+import { StreamingQuerystring } from "formidable/parsers";
 
 interface Props extends ModalProps {
   title?: string;
   onCancel?(): void;
 }
+
+const description: string =
+  "pupil to master is a snapshot of my journey through software development. I will showcase the things I'm learning, what I've built, and what's new in software. Feel free to join me on the journey!";
 
 const WelcomeModal: FC<Props> = ({
   visible,
@@ -22,11 +26,7 @@ const WelcomeModal: FC<Props> = ({
         <div className="dark:text-primary-dark text-primary font-semibold text-lg pb-2">
           {title}
         </div>
-        <p>
-          pupil to master is a snapshot of my journey through software
-          development. I will showcase the things I'm learning, what I've built,
-          and what's new in software. Feel free to join me on the journey!
-        </p>
+        <p>{description}</p>
       </div>
     </ModalContainer>
   );
